4135026350c06183f1133876b59e1471c708caae
kareemshahbaz7867 <kareemshahbaz@gmail.com>
2017-08-14 00:46:31 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile(createTemplate(articleOne));
+  res.send(createTemplate(articleOne));
 });
 
 
